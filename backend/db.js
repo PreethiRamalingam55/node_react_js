42fcf7c4216d577d4d4e48698359c1bf00ce6043
varun <varun@knewtontech.com>
2024-05-18 15:31:01 +0530

    db changed

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,26 @@
 // db.js
-
 const mysql = require('mysql2');
 
 require('dotenv').config();
 
 // Create a connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'New_Password123!',
-  database: process.env.DB_DATABASE || 'account',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Database connection failed:', err);
+  } else {
+    console.log('Database connection successful');
+    connection.release();
+  }
+});
 // Export the pool for use in your application
 module.exports = pool.promise();
