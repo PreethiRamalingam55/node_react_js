a93fc43e92e3c83c810528046730982a05d9817c
varun <varun@knewtontech.com>
2024-05-18 14:32:51 +0530

    env added

--- backend/db.js
+++ backend/db.js
@@ -2,12 +2,14 @@
 
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 // Create a connection pool
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'New_Password123!',
-  database: 'account',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'New_Password123!',
+  database: process.env.DB_DATABASE || 'account',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
